1259523c3b11b4b6d6193e233ab8b882399651fc
张坤 <1172878301@qq.com>
2018-10-09 18:38:46 +0800

    heap
    。。。

--- heap.go
+++ heap.go
@@ -1,8 +1,30 @@
 package algorithmSort
 
-func BuildHeap(arr []int) {
+func HeadSort(array []interface{}, cmp func(a, b interface{}) bool) {
+	len := len(array)
+	//建堆
+	for i := (len / 2) - 1; i >= 0; i-- {
+		AdjustHeap(array, i, len, cmp)
+	}
 
+	//排序
+	for i := 0; i < len; i++ {
+		sz := len - i - 1
+		array[0], array[sz] = array[sz], array[0]
+		AdjustHeap(array[0:sz], 0, sz, cmp)
+	}
 }
-func AdjustHeap(arr []int) {
 
+func AdjustHeap(array []interface{}, i, len int, cmp func(a, b interface{}) bool) {
+	for ch := i<<1 + 1; ch < len; ch = i<<1 + 1 {
+		rg := i<<1 + 2
+		if rg < len && cmp(array[ch], array[rg]) {
+			ch = rg
+		}
+		if cmp(array[ch], array[i]) {
+			return
+		}
+		array[ch], array[i] = array[i], array[ch]
+		i = ch
+	}
 }
--- sample/main.go
+++ sample/main.go
@@ -6,9 +6,43 @@ import "fmt"
 //插入   二分插入（折半查找  插入排序） 希尔排序
 //选择  直接选择  堆排序
 func main() {
-	arr := []int{2, 1, 3, 5, 4, 7, 6}
+	arr := []interface{}{2, 1, 3, 5, 4, 7, 6}
+	headSoft(arr, func(a, b interface{}) bool {
+		a1 := a.(int)
+		a2 := b.(int)
+		return a1 <= a2
+	})
+	fmt.Println(arr)
 
 	for _, v := range arr {
 		fmt.Println(v)
+	}
+}
+func headSoft(array []interface{}, cmp func(a, b interface{}) bool) {
+	len := len(array)
+	//建堆
+	for i := (len / 2) - 1; i >= 0; i-- {
+		adjustHeap(array, i, len, cmp)
+	}
+
+	//排序
+	for i := 0; i < len; i++ {
+		sz := len - i - 1
+		array[0], array[sz] = array[sz], array[0]
+		adjustHeap(array[0:sz], 0, sz, cmp)
+	}
+}
+
+func adjustHeap(array []interface{}, i, len int, cmp func(a, b interface{}) bool) {
+	for ch := i<<1 + 1; ch < len; ch = i<<1 + 1 {
+		rg := i<<1 + 2
+		if rg < len && cmp(array[ch], array[rg]) {
+			ch = rg
+		}
+		if cmp(array[ch], array[i]) {
+			return
+		}
+		array[ch], array[i] = array[i], array[ch]
+		i = ch
 	}
 }
